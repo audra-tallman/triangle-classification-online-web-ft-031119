3b36b6e33ca99640c7053cb687ca6bf3e70c348e
Audra Tallman <audi.tallman@gmail.com>
2019-04-01 20:36:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
-end
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a= a
+    @b= b
+    @c= c
+  end 
+  
+ 
+  def actual_triangle
+    triangle ? [(a + b > c), (b + c > a), (a + c > b)] &&
+      [a, b, c].each {|side| side >= 0} : puts TriangleError
+  end 
+ 
+  def kind 
+    actual_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || c == a
+      :isosceles
+    else 
+      :scalene
+    end
+  end 
+ 
+  class TriangleError < StandardError
+  end 
+end 
